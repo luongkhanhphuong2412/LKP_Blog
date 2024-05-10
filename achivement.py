237c5779c077bedbedddae6c6a4dd4bcd3918fea
luongkhanhphuong2412 <120470802+luongkhanhphuong2412@users.noreply.github.com>
2024-05-11 01:48:22 +0700

    Add new feature

--- achivement.py
+++ achivement.py
@@ -1,69 +1,76 @@
 import streamlit as st
-from PIL import Image
-image_certification1 = Image.open("images/DAA001.png")
-image_coursera_project_management = Image.open("images/PM.png")
-image_coursera_data_analytics= Image.open("images/Data.png")
-image_NUS_transcript= Image.open("images/NUS.png")
-
-
 
 def app():
-    with st.container():
-        st.title("MY ACHIEVEMENT🌤️")
-        st.write('##')
-        image_column, text_column = st.columns((0.5, 2))
-        
-        # Display the image in the first column
-        with image_column:
-            st.image(image_certification1)
-         # Display text in the second column
-        with text_column:
-            st.subheader("Associate Data Analyst in SQL")
-            st.write("Issued Apr 2024")
+    st.title("MY ACHIEVEMENT🌤️")
 
-    with st.container():
-        st.write('##')
-        image_column, text_column = st.columns((0.5, 2))
-        
-        # Display the image in the first column
-        with image_column:
-            st.image(image_coursera_project_management)
-         # Display text in the second column
-        with text_column:
-            st.subheader("Google Project Management Professional Certificate")
-            st.write("Issued Apr 2024")
+    st.write('## Associate Data Analyst in SQL')
+    st.write("Issued Apr 2024")
 
-    with st.container():
-        st.write('##')
-        image_column, text_column = st.columns((0.5, 2))
-        
-        # Display the image in the first column
-        with image_column:
-            st.image(image_coursera_data_analytics)
-         # Display text in the second column
-        with text_column:
-            st.subheader("Google Data Analytics Professional Certificate")
-            st.write("Issued Nov 2023")
+    # Create a row layout with two columns
+    image_column, text_column = st.columns((0.5, 2))
 
-    with st.container():
-        st.write('##')
-        image_column, text_column = st.columns((0.5, 2))
-        
-        # Display the image in the first column
-        with image_column:
-            st.image(image_NUS_transcript)
-         # Display text in the second column
-        with text_column:
-            st.subheader("NUS Enterprise Summer Programme")
-            st.write("Issued July 2023")
-            st.write(
-                """
-                - The NUS Enterprise Summer Programme is an immersive experience offered by the National University of Singapore (NUS) Enterprise,, where participants spend two weeks immersed in Singapore's vibrant entrepreneurial ecosystem.
-                - The program aimed at providing participants with valuable insights and practical knowledge in entrepreneurship and innovation.
-                - Key features of the NUS Enterprise Summer Programme often include: 
-                  + Entrepreneurship Workshops
-                  + Hands-on Projects
-                  + Networking Opportunities
-                  + Pitch Competitions
-                  + Cultural and Social Activities
-                """)
+    # Display the image in the left column
+    with image_column:
+        st.image("images/DAA001.png")
+
+    # Display text in the right column
+    with text_column:
+        st.subheader("Associate Data Analyst in SQL")
+        st.write("Issued Apr 2024")
+
+    st.write('## Google Project Management Professional Certificate')
+    st.write("Issued Apr 2024")
+
+    # Create a row layout with two columns
+    image_column, text_column = st.columns((0.5, 2))
+
+    # Display the image in the left column
+    with image_column:
+        st.image("images/PM.png")
+
+    # Display text in the right column
+    with text_column:
+        st.subheader("Google Project Management Professional Certificate")
+        st.write("Issued Apr 2024")
+
+    st.write('## Google Data Analytics Professional Certificate')
+    st.write("Issued Nov 2023")
+
+    # Create a row layout with two columns
+    image_column, text_column = st.columns((0.5, 2))
+
+    # Display the image in the left column
+    with image_column:
+        st.image("images/Data.png")
+
+    # Display text in the right column
+    with text_column:
+        st.subheader("Google Data Analytics Professional Certificate")
+        st.write("Issued Nov 2023")
+
+    st.write('## NUS Enterprise Summer Programme')
+    st.write("Issued July 2023")
+
+    # Create a row layout with two columns
+    image_column, text_column = st.columns((0.5, 2))
+
+    # Display the image in the left column
+    with image_column:
+        st.image("images/NUS.png")
+
+    # Display text in the right column
+    with text_column:
+        st.subheader("NUS Enterprise Summer Programme")
+        st.write("Issued July 2023")
+        st.write(
+            """
+            - The NUS Enterprise Summer Programme is an immersive experience offered by the National University of Singapore (NUS) Enterprise, where participants spend two weeks immersed in Singapore's vibrant entrepreneurial ecosystem.
+            - The program aimed at providing participants with valuable insights and practical knowledge in entrepreneurship and innovation.
+            - Key features of the NUS Enterprise Summer Programme often include: 
+              + Entrepreneurship Workshops
+              + Hands-on Projects
+              + Networking Opportunities
+              + Pitch Competitions
+              + Cultural and Social Activities
+            """
+        )
